efc35fc870e3fb4d012f9a54fcf64c212a74d760
fids20 <78724357+fids20@users.noreply.github.com>
2021-02-11 11:43:57 +0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -1,12 +1,10 @@
 let { WAConnection: _WAConnection, MessageType } = require('@adiwajshing/baileys')
-let WAConnection = require('../lib/simple').WAConnection(_WAConnection)
-let qrcode = require('qrcode')
 
 
 handler.help = ['jadibot']
 handler.tags = ['jadibot']
 handler.command = /^jadibot$/i
-handler.owner = false
+handler.owner = true 
 handler.mods = false
 handler.premium = false
 handler.group = false
